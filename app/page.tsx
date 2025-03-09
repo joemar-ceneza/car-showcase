97ffd4ff142b4336f6ae5525a42988e3f6758beb
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-03-09 11:36:55 +0800

    adding SearchBar and CustomFilter

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
-import { Hero } from "@/components";
+import { Hero, SearchBar, CustomFilter } from "@/components";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
-      <div></div>
+      <div className="max-width mt-12 padding-x padding-y" id="discover">
+        <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+        <p>Explore the cars you might like</p>
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter />
+            <CustomFilter />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
