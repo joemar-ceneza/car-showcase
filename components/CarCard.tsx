7f18f36759d9e51fafff193ab60fc7265d966676
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-03-18 21:17:52 +0800

    added CarCard components to display car details and rent calculation

---
+++ components/CarCard.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { CarProps } from "@/types";
+import CustomButton from "./CustomButton";
+import { calculateCarRent } from "@/utils";
+
+interface CarCardProps {
+  car: CarProps;
+}
+
+export function CarCard({ car }: CarCardProps) {
+  const { city_mpg, year, make, model, transmission, drive } = car;
+  const carRent = calculateCarRent(city_mpg, year);
+
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+        <p>
+          <span>Car Rent...</span>
+        </p>
+      </div>
+    </div>
+  );
+}
