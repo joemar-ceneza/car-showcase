0772938513469d2092dd695ca7f4063789971a46
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-03-22 15:46:54 +0800

    enhance CustomButton components with customizable text styles and right icon support

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -8,6 +8,8 @@ export default function CustomButton({
   containerStyles,
   handleClick,
   btnType,
+  textStyles,
+  rightIcon,
 }: CustomeButtonProps) {
   return (
     <button
@@ -15,7 +17,17 @@ export default function CustomButton({
       type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}>
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 }
